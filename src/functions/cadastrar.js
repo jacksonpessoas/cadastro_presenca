9f6c1d0a75153ab37da7a339b3d2f82f5055aac8
JacksonPessoaS <jacksonpessoas@gmail.com>
2024-10-15 20:30:56 -0300

    .

--- src/functions/cadastrar.js
+++ src/functions/cadastrar.js
@@ -3,14 +3,11 @@ const path = require('path');
 
 exports.handler = async function(event) {
     try {
-        // Log para verificar o corpo da requisição recebido
         console.log("Recebido o corpo da requisição:", event.body);
-        
-        // Tentativa de fazer parse do body como JSON
         const data = JSON.parse(event.body);
 
-        // Caminho onde o arquivo CSV será salvo
-        const filePath = path.join(__dirname, 'cadastros.csv');
+        // Caminho onde o arquivo CSV será salvo no diretório temporário
+        const filePath = path.join('/tmp', 'cadastros.csv');  // Alterado para /tmp
         const headers = 'Nome,Função,Contato,Endereço,Título,Zona,Seção,Líder\n';
 
         // Verificar se o arquivo CSV já existe, se não, cria com o cabeçalho
@@ -24,16 +21,13 @@ exports.handler = async function(event) {
         // Escrever no arquivo CSV
         fs.appendFileSync(filePath, linha);
 
-        // Retornar resposta de sucesso
         return {
             statusCode: 200,
             body: JSON.stringify({ message: 'Cadastro realizado com sucesso!' }),
         };
     } catch (error) {
-        // Log de erro no processamento
         console.error('Erro ao processar a requisição:', error);
 
-        // Retornar resposta de erro
         return {
             statusCode: 400,
             body: JSON.stringify({ message: 'Erro ao processar o cadastro' }),
