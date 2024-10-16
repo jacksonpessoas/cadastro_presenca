b562f40fef1445525df6c67b0c1902f407af1a1d
JacksonPessoaS <jacksonpessoas@gmail.com>
2024-10-15 21:12:41 -0300

    atualizado

--- src/functions/cadastrar.js
+++ src/functions/cadastrar.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-exports.handler = async function (event) {
+exports.handler = async function(event) {
     try {
         console.log("Recebido o corpo da requisição:", event.body);
         const data = JSON.parse(event.body);
 
-        // Caminho onde o arquivo CSV será salvo no diretório temporário
-        const filePath = path.join('/tmp', 'cadastros.csv');  // Usando o diretório temporário /tmp
+        // Caminho onde o arquivo CSV será salvo no diretório temporário /tmp
+        const filePath = path.join('/tmp', 'cadastros.csv');  // Certifique-se de que o caminho é /tmp
         const headers = 'Nome,Função,Contato,Endereço,Título,Zona,Seção,Líder\n';
 
         // Verificar se o arquivo CSV já existe, se não, cria com o cabeçalho
